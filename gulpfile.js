0ee400234ef79e7dee10c6700ee3f9e7d4d3fd66
matidiaz032 <maty_032@hotmail.es>
2021-05-13 19:52:42 -0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,33 @@
 const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
-const imagemin  = require('gulp-imagemin');
+/* const imagemin  = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const webp = require('gulp-webp');
-const concat = require('gulp-concat');
+const concat = require('gulp-concat'); */
 
 // Utilidades CSS
-const autoprefixer = require('autoprefixer');
+/* const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
-const sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps'); */
 
 // Utilidades JS
-const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
+/* const terser = require('gulp-terser-js');
+const rename = require('gulp-rename'); */
 
-const paths = {
-    imagenes: 'src/img/**/*',
-    scss: 'src/scss/**/*.scss',
-    js: 'src/js/**/*.js'
-}
+/*  const paths = { */
+   // imagenes: 'src/img/**/*',
+   // scss: 'src/scss/**/*.scss',
+   // js: 'src/js/**/*.js'
+/* } */
 
 function css() {
-    return src(paths.scss)
-        .pipe( sourcemaps.init() )
+    return src('src/scss/app.scss')
         .pipe( sass())
-        .pipe( postcss([ autoprefixer(), cssnano() ]))
-        .pipe( sourcemaps.write('.') )
         .pipe( dest('./build/css') )
 }
 
-function javascript() {
+/* function javascript() {
     return src(paths.js)
         .pipe(sourcemaps.init())
         .pipe( concat('bundle.js') )
@@ -38,29 +35,37 @@ function javascript() {
         .pipe(sourcemaps.write('.'))
         .pipe( rename({ suffix: '.min' }))
         .pipe( dest('./build/js') )
-}
+} */
 
-function imagenes() {
+/* function imagenes() {
     return src(paths.imagenes)
         .pipe( imagemin() )
         .pipe( dest( './build/img' ))
         .pipe( notify({ message: 'Imagen Minificada'}) );
-}
+} */
 
-function versionWebp() {
+/* function versionWebp() {
     return src(paths.imagenes)
         .pipe( webp() )
         .pipe( dest('./build/img'))
         .pipe( notify({message: 'Versión webP lista'}));
+} */
+
+function minificarcss() {
+    return src('src/scss/app.scss')
+    .pipe( sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe( dest('.build/css') )
 }
 
 function watchArchivos() {
-    watch( paths.scss, css ); // * = La carpeta actual - ** = Todos los archivos con esa extensión
-    watch(paths.js, javascript);
+    watch( 'src/scss/**/*.scss', css );
 }
 
 exports.css = css;
-exports.imagenes = imagenes;
+/* exports.imagenes = imagenes; */
+exports.minificarcss = minificarcss;
 exports.watchArchivos = watchArchivos;
 
-exports.default = series( css, javascript, imagenes, versionWebp, watchArchivos );
+/* exports.default = series( css, javascript, imagenes, versionWebp, watchArchivos ); */
